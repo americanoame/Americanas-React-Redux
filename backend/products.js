3893ba86cc24b98cef0aa2afadfa15ce615da965
Americano.Dev <andrenations@gmail.com>
2024-06-09 18:26:46 -0400

    add product data for kitchen, bathroom, and room categories

---
+++ backend/products.js
@@ -0,0 +1,49 @@
+const products = [
+  {
+    id: '5968897c-4d27-4872-89f6-5bcb052746d7',
+    image: '/products/toaster.jpg',
+    name: "MUELLER RETRO toaster 2 Slice",
+    rating: {
+      stars: 4.5,
+      count: 789,
+    },
+    price: 2070,
+    category: 'kitchen',
+  },
+
+  {
+    id: 'b0f17cc5-8b40-4ca5-9142-b61fe3d98c85',
+    image: '/products/trash-can.jpg',
+    name: "TRASH CAN with foot pedal 50 liter",
+    rating: {
+      stars: 4.5,
+      count: 1501,
+    },
+    price: 1374,
+    category: 'bathroom',
+  },
+
+  {
+    id: 'a45cfa0a-66d6-4dc7-9475-e2b01595f7d7',
+    image: '/products/vanity-mirror-silver.jpg',
+    name: "SILVER DOULBE sided Mirror, 360°",
+    rating: {
+      stars: 4.5,
+      count: 3112,
+    },
+    price: 2400,
+    category: 'room',
+  },
+];
+
+
+
+
+
+
+
+
+
+export default products;
+
+
