a22858a26050a2dd57d5f9e00cf9efa12d6d75bc
Americano.Dev <andrenations@gmail.com>
2024-06-09 18:37:50 -0400

    set up Redux Toolkit query API for fetching products

---
+++ frontend/src/redux/productsApi.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const productsApi = createApi({
+    reducerPath: 'productsApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3002'}),
+    endpoints: (buider) => ({
+        getAllProducts: buider.query({
+            query: () => 'products',
+        }),
+    }),
+});
+
+export const { useGetAllProductsQuery } = productsApi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
