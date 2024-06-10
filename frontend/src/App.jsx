452cc21436916271800a5c4729846954775fef73
Americano.Dev <andrenations@gmail.com>
2024-06-10 19:47:28 -0400

    Add WishList component route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home'
+import Home from './components/home/Home';
+import WishList from './components/wishlist/WishList';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/wishlist" element={<WishList />} />
         
       </Routes>
     </BrowserRouter>
