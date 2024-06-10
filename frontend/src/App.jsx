0f5ea95c924d542274ed4b43c47115b0a5c58341
Americano.Dev <andrenations@gmail.com>
2024-06-09 22:49:11 -0400

    add ToastContainer to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-    
+    <ToastContainer/>
       <Navbar />
       <Routes>
       <Route exact path="/" element={<Home />} />
