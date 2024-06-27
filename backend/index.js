eb10b7ba0c59d7ecde42a0eb1c4a3c6ee628ceb2
Americano.Dev <andrenations@gmail.com>
2024-06-27 17:42:35 -0400

    backend serve frontend

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,14 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 app.get('/products', (req, res) => {
     res.send(products)
 })
 
-app.get('/', (req, res) => {
-  res.send('e-commerce');
-});
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+} )
 
 app.listen(port, console.log(`Server Listening on port ${port}`));
