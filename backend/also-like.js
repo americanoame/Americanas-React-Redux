5b5ef4828592f6af687bdd98d8777290de5188c9
Americano.Dev <andrenations@gmail.com>
2024-06-27 16:38:33 -0400

    Add alsoLike data with product details and images

---
+++ backend/also-like.js
@@ -0,0 +1,34 @@
+const alsoLike = [
+    {
+      id: '8a1b2c3d-4e5f-6g7h-8i9j-0k1l2m3n4o5p',
+      image: '/also-like-image/toast.webp',
+      name: "LOCKnLOCK Aqua Fridge",
+      rating: {
+        stars: 4.2,
+        count: 120,
+      },
+      price: 799,
+    },
+    {
+      id: '9b2c3d4e-5f6g-7h8i-9j0k-1l2m3n4o5p6q',
+      image: '/also-like-image/coffeemaker.jpg',
+      name: "12-CUP DIGITAL Coffee Maker",
+      rating: {
+        stars: 4.7,
+        count: 310,
+      },
+      price: 1299,
+    },
+    {
+      id: 'ac3d4e5f-6g7h-8i9j-0k1l-2m3n4o5p6q7r',
+      image: '/also-like-image/paper.jpg',
+      name: "KITCHEN paper-towels 30 pack",
+      rating: {
+        stars: 4.3,
+        count: 220,
+      },
+      price: 1599,
+    },
+  ];
+  
+ export default alsoLike;
