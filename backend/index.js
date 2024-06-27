64951390a6826369861aa92aa4c8e12457e16e07
Americano.Dev <andrenations@gmail.com>
2024-06-27 19:37:17 -0400

    Add cors configuration for development environment

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import products from './products.js';
-
 import cors from 'cors';
+
+
 
 const port = process.env.PORT || 3002;
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://127.0.0.1:5175',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
+
 
 app.use(express.json());
 app.use(express.static( "../frontend/dist"))
