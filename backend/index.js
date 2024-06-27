5e84b106c5d6b17ebf112e17f1c9e6ea91c8dd26
Americano.Dev <andrenations@gmail.com>
2024-06-27 17:50:28 -0400

    remove path

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
+app.use(express.static( "../frontend/dist"))
 
 app.get('/products', (req, res) => {
     res.send(products)
 })
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  res.sendFile( "../frontend/dist/index.html")
 } )
 
 app.listen(port, console.log(`Server Listening on port ${port}`));
