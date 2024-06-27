1eb9a4a7a901a5aec0cf1c6469aeef1a2d1520af
Americano.Dev <andrenations@gmail.com>
2024-06-27 16:41:40 -0400

    Add Cart component and integrate it into the routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
+import Cart from './components/cart/Cart';
 import WishList from './components/wishlist/WishList';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/wishlist" element={<WishList />} />
-        
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/wishlist" element={<WishList />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
