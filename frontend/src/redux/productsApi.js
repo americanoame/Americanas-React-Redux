e5ea146831055f55fedb89366fcd6dfe80c4e967
Americano.Dev <andrenations@gmail.com>
2024-08-25 23:08:41 -0400

    fix the base url

--- frontend/src/redux/productsApi.js
+++ frontend/src/redux/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const productsApi = createApi({
     reducerPath: 'productsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3002/'}),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3002' }),
     endpoints: (buider) => ({
         getAllProducts: buider.query({
             query: () => 'products',
